e4cfe701cea4c036292a24b44aea44914797cd66
Mishaal <mishaalcanvawali1@gmail.com>
2025-10-21 22:39:37 +0500

    Create templates.php

---
+++ templates.php
@@ -0,0 +1,10 @@
+<?php
+// Example templates
+$templates = [
+    "Summarize Text" => "Please summarize the following text:",
+    "Email Writing" => "Write a professional email for:",
+    "Code Generation" => "Generate PHP code for:",
+    "Blog Idea" => "Create blog ideas about:",
+    "Translation" => "Translate this to English:"
+];
+?>
